7778e71dca06c9a99fe8a9865b206d9872affc8a
Vinícius <107704540+epicestudar@users.noreply.github.com>
2023-06-03 02:25:09 +0000

    navbar + menu carrossel

---
+++ ProjetoFinalSemestre/home.js
@@ -0,0 +1,48 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+    slider.forEach(item => item.classList.remove('on'));
+}
+
+function showSlider() {
+    slider[currentSlide].classList.add('on');
+}
+
+function nextSlider() {
+    hideSlider()
+        if(currentSlide === slider.length - 1) {
+            currentSlide = 0;
+        }
+        else {
+            currentSlide++;
+        }
+        showSlider()
+}
+
+function prevSlider() {
+    hideSlider()
+        if(currentSlide === 0) {
+            currentSlide = slider.length -1;
+        }
+        else {
+            currentSlide--;
+        }
+        showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider);
+btnPrev.addEventListener('click', prevSlider);
+
+console.log(slider)
