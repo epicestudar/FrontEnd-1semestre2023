572ee13065de9bbba5092d8ffa57fbe4c97305fa
Vinícius <107704540+epicestudar@users.noreply.github.com>
2023-06-08 21:02:14 +0000

    jksdjkdsjdks

--- ProjetoFinalSemestre/home.js
+++ ProjetoFinalSemestre/home.js
@@ -46,3 +46,22 @@ btnNext.addEventListener('click', nextSlider);
 btnPrev.addEventListener('click', prevSlider);
 
 console.log(slider)
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
